dbc1edad98370d1d0087a5f8bde782d2efc873f0
DESKTOP-9D9GPOO\zaman <zamank24@hotmail.com>
2017-09-19 20:43:19 -0600

    added all files to complete project

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	//Click event to scroll to top
+		$('.navbar-brand').click(function(){
+			$('html, body').animate({scrollTop : 0},800);
+			return false;
+		});
+
+  // Add smooth scrolling on all links inside the navbar
+  $(".nav a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
